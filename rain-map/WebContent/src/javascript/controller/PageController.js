cae3261743f47b89d5b07d577d324bd0a7bf268f
tkashi <kashimura.takuya.ca2@jp.nssol.nssmc.com>
2014-11-26 09:21:07 +0900

    ファイルヘッダにCopyrightとファイルヘッダを追記

--- rain-map/WebContent/src/javascript/controller/PageController.js
+++ rain-map/WebContent/src/javascript/controller/PageController.js
@@ -1,15 +1,21 @@
 /*
  * Copyright (C) 2014 NS Solutions Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 (function($) {
 	/**
-	 *
-	 *
-	 *  parse後のデータは次の形式とする。
-	 *  {
-	 *  	date1: {MapKey1: value1, MapKey2: value2,....}
-	 *  	date2: {MapKey1: ...
-	 *  }
+	 * parse後のデータは次の形式とする。 { date1: {MapKey1: value1, MapKey2: value2,....} date2: {MapKey1: ... }
 	 */
 	var PageController = {
 
@@ -136,8 +142,7 @@
 				var speed = this.SpeedController.getSpeed();
 				var interval = 1000 / speed;
 				this.play(interval);
-			}
-			else {
+			} else {
 				this.pause();
 			}
 		},
